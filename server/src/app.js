a5664ccfbc4344712a2f33d91021f1f26fd417f9
hoo00nn <hoo00nn@naver.com>
2021-01-11 20:40:20 +0900

    Chore #noissue : express 서버 구성
    - express로 서버 설정

---
+++ server/src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.listen(PORT, () => console.log("server start.."));
