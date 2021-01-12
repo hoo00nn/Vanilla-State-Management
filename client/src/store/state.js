b9374c5e1152c1fdd60cfd40c5fd1048033144a1
hoo00nn <hoo00nn@naver.com>
2021-01-12 13:38:40 +0900

    Feat #noissue : 초기 상태 세팅

---
+++ client/src/store/state.js
@@ -0,0 +1,13 @@
+export default {
+  items: [
+    {
+      item: "GraphQL 학습하기",
+      done: false,
+    },
+    {
+      item: "NestJS 학습하기",
+      done: false,
+    },
+  ],
+  status: "ALL",
+};
