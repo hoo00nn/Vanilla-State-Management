95dc77fc5e4c0809afe98980ad9c92a72f40fc3c
hoo00nn <hoo00nn@naver.com>
2021-01-12 13:36:17 +0900

    Chore #noissue : 정적 파일 경로 설정 추가

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const path = require("path");
 
 const app = express();
 
@@ -7,5 +8,6 @@ const PORT = 3000;
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../../client/src")));
 
 app.listen(PORT, () => console.log("server start.."));
