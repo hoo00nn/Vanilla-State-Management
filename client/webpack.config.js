4cb7272f4fd6e74d359c56f423452933ccdf54b3
hoo00nn <hoo00nn@naver.com>
2021-01-14 22:27:28 +0900

    Chore #noissue : 기본적인 웹팩 설정

---
+++ client/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const htmlWepackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/app.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
